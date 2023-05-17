0491decfe0222c1aea8b43bdaad072f72b1ed598
Daniel Durán <danielduranje@outlook.com>
2023-05-17 18:24:09 -0500

    first commit

---
+++ public/js/nuevo-ticket.js
@@ -0,0 +1,24 @@
+const lblNuevoTicket = document.querySelector("#lblNuevoTicket");
+const btnCrear = document.querySelector("button");
+
+const socket = io();
+
+socket.on("connect", () => {
+  // console.log('Conectado');
+  btnCrear.disabled = false;
+});
+
+socket.on("ultimo-ticket", (ultimoTicket) => {
+  lblNuevoTicket.innerText = "Ticket " + ultimoTicket;
+});
+
+socket.on("disconnect", () => {
+  // console.log('Desconectado del servidor');
+  btnCrear.disabled = true;
+});
+
+btnCrear.addEventListener("click", () => {
+  socket.emit("siguiente-ticket", null, (ticket) => {
+    lblNuevoTicket.innerText = ticket;
+  });
+});
